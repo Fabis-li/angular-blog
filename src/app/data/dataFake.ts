71219b13cdbd558e1166d0e59ece5111a68f6e1b
Fabiano <fabiano_slima@hotmail.com>
2023-05-19 16:15:13 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake =[
+    {
+        "id": "1",        
+        "title": "Novo homem de ferro",
+        "description": "Marvel anuncia novo filme do homem de ferro",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqVXOItZ-LQem-1m1IzfQzYrQf0-tqzocXi4h2W541EHRQ3sqIXeSh4WIzcGt-6pZw_aw&usqp=CAU"
+    },
+    {
+        "id": "2",        
+        "title": "Nova serie Disney+",
+        "description": "ola",
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    }
+]
