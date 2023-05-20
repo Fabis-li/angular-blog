bc4e621cf10e0672ac18b674d5f143648a0450d7
Fabiano <fabiano_slima@hotmail.com>
2023-05-20 00:21:51 -0300

    image exchange

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,10 +3,22 @@ export const dataFake =[
         "id": "1",        
         "title": "Novo homem de ferro",
         "description": "Marvel anuncia novo filme do homem de ferro",
-        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqVXOItZ-LQem-1m1IzfQzYrQf0-tqzocXi4h2W541EHRQ3sqIXeSh4WIzcGt-6pZw_aw&usqp=CAU"
+        "photoCover": "https://uploads.metropoles.com/wp-content/uploads/2021/09/21161952/Patrulha_Canina_Dia_das_Criancas.jpg"
     },
     {
         "id": "2",        
+        "title": "Chase",
+        "description": "É um pastor alemão reponsavel e o mais sério dos filhotes, embora seja bastante brincalhão quando estaestá fora das missões. Chase e um lider nato e tem habilidades de um cão policial, mas também atua como agente de trânsito e espião. Ele dirige um carro de polícia azul que se transforma em um veículo de espionagem. Suas principais frases são 'Chase está no caso!' e a 'Patrulha Canina pronta pra ação!'",
+        "photoCover": "https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/3/AmazonStores/A2Q3Y263D00KWC/af7032c28c97beb696e29080667221df.w2451.h1988._CR0%2C0%2C2451%2C1988_SX1500_.jpg"
+    },
+    {
+        "id": "3",        
+        "title": "Marshall",
+        "description": "Esse dalmata adora ação e é também o mais desastrado dos filhotes. Ele fica tão animado com as missões  que sempre acaba derrubando coisas e se acidentando> Marshall dirige uma caminhão de bombeiros que se transforma em ambulância. Sua frase mais famosa é 'Eu estou bem!', que faz so pequenos cairem na risada toda vez que se atrapalha em alguma aventura.",
+        "photoCover": "https://m.media-amazon.com/images/S/stores-image-uploads-na-prod/9/AmazonStores/A2Q3Y263D00KWC/79e2bfd46ffbe843bbb101ffd61ea78f.w2451.h1988._CR0%2C0%2C2451%2C1988_SX1500_.jpg"
+    },
+    {
+        "id": "4",        
         "title": "Nova serie Disney+",
         "description": "ola",
         "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
